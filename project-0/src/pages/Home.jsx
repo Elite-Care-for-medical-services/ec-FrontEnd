b51513f103227495558692756349a41b1cd05d44
Moniaar <www.omniaahmed1562001@gmail.com>
2025-06-17 20:44:45 +0300

    converted the warapping container to a React fragment

--- project-0/src/pages/Home.jsx
+++ project-0/src/pages/Home.jsx
@@ -2,11 +2,11 @@ import {Link} from 'react-router-dom';
 
 export default function Home() {
 	return (
-		<div>
+		<>
 		<h1>Team Members</h1>
 		<Link to="/">Go to the landing page</Link>
 		<br />
 		<Link to="/team">Meet the team</Link>
-		</div>
+		</>
 	);
 }
