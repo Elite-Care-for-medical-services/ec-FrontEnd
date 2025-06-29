a0729b79e01d40ae579ebe466bad57d6ece7cba0
Moniaar <www.omniaahmed1562001@gmail.com>
2025-06-29 22:50:15 +0300

    have mock data inside

---
+++ project-0/src/store/teamMockData.ts
@@ -0,0 +1,15 @@
+// Has all our data for the team members
+
+const teamMembers = [
+  { name: 'Aymen', role: 'CEO', github: 'https://github.com/Fandam11', linkedin: 'https://www.linkedin.com/in/aymen-fandam-1b0a4b1b6/' },
+  { name: 'Hammam AbdElmajed', role: 'Frontend Engineer', github: 'https://github.com/Hammamabdelmajed', linkedin: 'https://www.linkedin.com/in/hammam-abdelmajed-4b0a1b1b6/' },
+  { name: 'Modather Abd Alhag', role: 'Frontend Engineer', github: 'https://github.com/modtherhub', linkedin: 'https://www.linkedin.com/in/modather-abd-alhag-4b0a1b1b6/' },
+  { name: 'Omnia Ahmed', role: 'Frontend Engineer', github: 'https://github.com/Moniaar', linkedin: 'https://www.linkedin.com/in/omnia-ahmed-4b0a1b1b6/' },
+  { name: 'Abd Elaziz Mohsen', role: 'Backend Developer', github: 'https://github.com/Batman99SD', linkedin: 'https://www.linkedin.com/in/abd-elaziz-mohsen-4b0a1b1b6/' },
+  { name: 'Mohamed Musa', role: 'Backend Engineer', github: 'https://github.com/Kusa-san', linkedin: 'https://www.linkedin.com/in/mohamed-musa-4b0a1b1b6/' },
+  { name: 'Omer Alfaroug', role: 'DevOps', github: 'https://github.com/Mr-Robinhood', linkedin: 'https://www.linkedin.com/in/omer-alfaroug-4b0a1b1b6/' },
+  { name: 'Ali Hassan', role: 'DevOps', github: 'https://github.com/A1iHassan', linkedin: 'https://www.linkedin.com/in/ali-hassan-4b0a1b1b6/' },
+];
+
+
+export default teamMembers;
